6279bcdb4918a81a28890d8a0f2ce243dd5e5a47
Said Demir <saiddemir98@gmail.com>
2022-11-28 19:34:39 +0300

    firs commit

---
+++ src/main/java/com/softtcode/turnuva/TurnuvaApplication.java
@@ -0,0 +1,13 @@
+package com.softtcode.turnuva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnuvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnuvaApplication.class, args);
+	}
+
+}
